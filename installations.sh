4a49b4e00f1411eef871d31e5223cb363e61e588
harinadh kamma <harinadkamma@gmail.com>
2025-09-23 06:08:43 +0530

    odd

--- installations.sh
+++ installations.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 userid=$(id -u)
-if[ $userid -ne 0 ]; then
+if [ $userid -ne 0 ]; then
     echo "user don't have root access to install"
 fi
 dnf install mysql -y
-if[ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "installing  mysql is sucess"
 else
     echo "installing mysql is failed"
